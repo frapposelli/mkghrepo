2db307475376718427e2e58c2f16dd4062d5b3d1
Fabio Rapposelli <fabio@vmware.com>
2015-12-18 17:35:53 -0800

    add team suffix, team members get maintainer status, make teams public

--- lib/mkghrepo/repo.rb
+++ lib/mkghrepo/repo.rb
@@ -1,16 +1,18 @@
 # encoding: UTF-8
 require 'octokit'
 
+PREVIEW_API_HEADER = 'application/vnd.github.ironman-preview+json'
+
 module Mkghrepo
   # Repo provides an interface to github repos
   class Repo
     def initialize(token)
-      # stack = Faraday::RackBuilder.new do |builder|
-      #   builder.response :logger
-      #   builder.use Octokit::Response::RaiseError
-      #   builder.adapter Faraday.default_adapter
-      # end
-      # Octokit.middleware = stack
+      stack = Faraday::RackBuilder.new do |builder|
+        builder.response :logger
+        builder.use Octokit::Response::RaiseError
+        builder.adapter Faraday.default_adapter
+      end
+      Octokit.middleware = stack
       @client = Octokit::Client.new(access_token: token)
     end
 
@@ -32,11 +34,15 @@ module Mkghrepo
       @client.create_team(organization,
                           name: team_name,
                           repo_names: [repo],
-                          permission: permissions)
+                          permission: permissions,
+                          privacy: 'closed',
+                          accept: PREVIEW_API_HEADER)
     end
 
     def add_user_to_team(user, team_id)
-      @client.add_team_member(team_id, user)
+      @client.add_team_membership(team_id, user,
+                              role: 'maintainer',
+                              accept: PREVIEW_API_HEADER)
     end
   end
 end
